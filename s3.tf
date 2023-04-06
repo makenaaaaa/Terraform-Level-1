4b0d567d2829cd62dfd511e795675d18f5c6254a
EC2 Default User <ec2-user@ip-172-31-79-22.ec2.internal>
2023-04-06 08:51:45 +0000

    add s3 static website

---
+++ s3.tf
@@ -0,0 +1,22 @@
+resource "aws_s3_bucket" "b" {
+  bucket = "makenatest20230406"
+  acl    = "public-read"
+  website {
+    index_document = "index.html"
+  }
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET","PUT","POST", "DELETE","HEAD"]
+    allowed_origins = ["*"]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket_object" "file_upload" {
+  bucket = aws_s3_bucket.b.id
+  key    = "index.html"
+  source = "index.html"
+  content_type = "text/html"
+  acl    = "public-read"
+}
